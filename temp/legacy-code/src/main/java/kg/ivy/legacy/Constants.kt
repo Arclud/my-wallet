a9a4b413467ccada99902c0be0b607b9b20921da
Rustam <rustambakytov7@gmail.com>
2024-11-11 16:38:02 +0600

    Corrected Urls

--- temp/legacy-code/src/main/java/kg/ivy/legacy/Constants.kt
+++ temp/legacy-code/src/main/java/kg/ivy/legacy/Constants.kt
@@ -2,17 +2,17 @@ package kg.ivy.legacy
 
 object Constants {
     const val URL_TC =
-        "https://github.com/ILIYANGERMANOV/privacy-policies/blob/master/ivy-wallet-tc.md"
+        "https://github.com/Arclud/privacy-policies/blob/master/ivy-wallet-tc.md"
     const val URL_PRIVACY_POLICY =
-        "https://github.com/ILIYANGERMANOV/privacy-policies/blob/master/ivy-wallet-privacy-policy.md"
+        "https://github.com/Arclud/privacy-policies/blob/master/ivy-wallet-privacy-policy.md"
 
-    const val URL_IVY_WALLET_REPO = "https://github.com/Ivy-Apps/ivy-wallet"
+    const val URL_IVY_WALLET_REPO = "https://github.com/Arclud/my-wallet"
 
-    const val URL_GITHUB_NEW_ISSUE = "https://github.com/Ivy-Apps/ivy-wallet/issues/new/choose"
+    const val URL_GITHUB_NEW_ISSUE = "https://github.com/Arclud/my-wallet/issues/new/choose"
 
-    const val URL_HELP_CENTER = "https://t.me/+ETavgioAvWg4NThk"
+    const val URL_HELP_CENTER = "https://t.me/2393151507/11"
 
-    const val URL_IVY_TELEGRAM_INVITE = "https://t.me/+ETavgioAvWg4NThk"
+    const val URL_IVY_TELEGRAM_INVITE = "https://t.me/2393151507/1"
 
     const val URL_IVY_WALLET_GOOGLE_PLAY =
         "https://play.google.com/store/apps/details?id=kg.ivy.wallet"
